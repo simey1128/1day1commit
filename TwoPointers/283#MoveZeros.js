fcc9d421a49c3c4d984be521d3a7c0050607abc9
simey <simey1128@gmail.com>
2021-11-30 10:57:36 +0900

    [#1] Array에서 특정 element만 모으기

---
+++ TwoPointers/283#MoveZeros.js
@@ -0,0 +1,51 @@
+//in-place
+
+/*
+1. using Array.splice
+*/
+
+let movezeros = function (nums) {
+  const max = nums.length;
+  for (let i = max - 1; i >= 0; i--) {
+    if (!nums[i]) {
+      nums.splice(i, 1); // nums의 i index를 제거한다.
+      nums.push(0);
+    }
+  }
+  return nums;
+};
+
+/*
+2. using two pointers
+pointer 'a' = iterating whole array
+pointer 'b' = for zero elements
+*/
+
+movezeros = function (nums) {
+  const max = nums.length;
+  let pos = 0; // pointer
+  for (let i = 0; i < max; i++) {
+    if (nums[i] !== 0) {
+      nums[pos] = nums[i];
+      pos++;
+    }
+  }
+  for (let i = pos; i < max; i++) {
+    nums[i] = 0;
+  }
+};
+
+/*
+pointer idx = for non zero elements
+pointer i = iterate whole array
+ */
+movezeros = function (nums) {
+  var idx = 0;
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[idx] = nums[i];
+      nums[i] = idx === i ? nums[i] : 0;
+      idx++;
+    }
+  }
+};
